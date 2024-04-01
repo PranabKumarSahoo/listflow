097b6774179603a1807c63f6965d827b09d1fb5b
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-02 00:13:12 +0530

    mongoose schema and model created

---
+++ server/model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    list: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'List',
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema);
