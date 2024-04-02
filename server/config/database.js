1280151f5af82be88a4c435ef3f607475dd5b4a2
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-02 21:23:43 +0530

    [update]: minor changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
+const url = `mongodb+srv://pranab90:pranab90783@cluster0.0opmf0k.mongodb.net/`;
+
 const connectDatabase = async (req, res) => {
     try {
         await mongoose
-            .connect("mongodb+srv://pranab90:pranab90783@cluster0.0opmf0k.mongodb.net/")
+            .connect(url)
             .then(() => {
                 console.log("Database Connected!!");
             });
     } catch (error) {
-        res.status(400).json({
-            message: "Database Not Connected!!!"
-        });
+        console.error("Database Connection Failed");
     }
 };
 connectDatabase();
