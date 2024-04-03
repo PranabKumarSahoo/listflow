76e91f139270c1029e131d007e5c4dc32cafce63
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-03 23:58:26 +0530

    [enhance]: design About Page

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <>
+            <div className='about d-flex flex-column justify-content-center align-items-center'>
+                <div className="container text-white text-center d-flex flex-column gap-5">
+                    <h1>About Us</h1>
+                    <p>
+                        <q>
+                            ListFlow is your ultimate task management solution, designed to simplify your workflow and boost your productivity. With ListFlow, you can effortlessly create, organize, and prioritize tasks, ensuring that you stay focused on what matters most. This tool is built to adapt to your unique needs, whether you're a professional or a student.
+                        </q>
+                    </p>
+                    <img src="https://i.ibb.co/Kh6nzhg/about-us.png" className='about-img' alt="AboutUs Todo" />
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default About;
