888b8a4136f64dd0796fbd6ef57fba7a2a2225a9
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-03 23:52:57 +0530

    [enhance]: design footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Footer.css';
+import { FiArrowUpRight } from "react-icons/fi";
+
+
+const Footer = () => {
+    return (
+        <div className='footer container-fluid p-4 text-white'>
+            <div className="container d-flex justify-content-between align-items-center">
+                <div className="footer-title">
+                    <a href='/'>ListFlow</a>
+                </div>
+                <div className='footer-copyright'>
+                    Developed by <a href="https://pranabks.vercel.app" target='_blank' rel="noreferrer">Pranab KS</a>
+                </div>
+                <div className='social-links'>
+                    <a href="https://github.com/PranabKumarSahoo/listflow" target='_blank' rel="noreferrer">
+                        GitHub <FiArrowUpRight />
+                    </a>
+                    <a href="https://twitter.com/PranavvvKS" target='_blank' rel="noreferrer">
+                        Twitter <FiArrowUpRight />
+                    </a>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Footer;
