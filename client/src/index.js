ed351a0f396237bdb726bca8241b412d70000f0d
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-06 11:25:17 +0530

    [feat]: implement redux feature for state management

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // provide the Redux store to all components in the application.
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
