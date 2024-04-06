615e62d65654d9acfa1b48cd9628a8cb56148336
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-06 11:40:34 +0530

    [feat]: implement authentication check using redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -8,8 +8,20 @@ import SignUp from './components/SignUp/SignUp';
 import LogIn from './components/LogIn/LogIn';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from './store';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const id = sessionStorage.getItem('id');
+    if (id) {
+      dispatch(authActions.login());
+    }
+  }, [dispatch]);
+
   return (
     <div>
       <BrowserRouter>
