a22d36758ca41e0189513be10c36a4469b1c6898
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-06 11:22:16 +0530

    [feat]: implement cors policy

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+
 require('./config/database');
 
 const auth = require('./routes/auth');
 const list = require('./routes/list');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello, world!!");
@@ -19,6 +22,7 @@ app.use('/api/v1', auth);
 // setting up a middleware function for the todo list
 app.use('/api/v2', list);
 
+// starting the server that listens on port 8000.
 app.listen(8000, () => {
     console.log("Server Started!!!");
 });
