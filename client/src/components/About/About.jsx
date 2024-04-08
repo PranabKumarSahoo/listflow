41f533f52507ba412783366b8375f80064d1abfc
PranabKumarSahoo <kumarspranab19@gmail.com>
2024-04-08 14:47:28 +0530

    [update]: minor text change

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     <h1>About Us</h1>
                     <p>
                         <q>
-                            ListFlow is your ultimate task management solution, designed to simplify your workflow and boost your productivity. With ListFlow, you can effortlessly create, update, and delete tasks, ensuring that you stay focused on what matters most. This tool is built to adapt to your unique needs, whether you're a professional or a student.
+                            ListFlow is your ultimate task management solution, designed to simplify your workflow and boost your productivity. With ListFlow, you can effortlessly create, update and delete tasks, ensuring that you stay focused on what matters most. This tool is built to adapt to your unique needs, whether you're a professional or a student.
                         </q>
                     </p>
                     <img src="https://i.ibb.co/Kh6nzhg/about-us.png" className='about-img' alt="AboutUs Todo" />
